45b9ea1c1309a13adf141c05b82abfae1ff28614
saket <jhasaket99dbg@gmail.com>
2025-04-04 15:28:01 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "./components/sections/Hero";
 import Features from "./components/sections/Feature";
+import img from "./assets/images/hero.png"
 export default function Home() {
   return (
     <>
       <Hero
-        imgSrc="/images/hero.jpg"
+        imgSrc={img}
         h1Content="Predict the future by <br /> creating it"
         pContent="YOU DIDN'T COME THIS FAR TO STOP."
       />
